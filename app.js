f03a4d09626264f9d19a2726e58daafb6c971f95
DanielJunkes <danielmafioleti2@gmail.com>
2023-04-18 16:12:23 -0300

    ..

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express(); // criando o express app
 app.use(express.json()); // parsear o body do request como JSON
 app.use(express.urlencoded({ extended: false })); // parsear o body do request como query string
 app.use(cookieParser()); // parsear os cookies
-app.use(express.static("./public")) // pegar os css
+app.use(express.static("./public")) // pegar os css funcoes
 
 // adicionando rotas
 app.use("/", indexRouter);
