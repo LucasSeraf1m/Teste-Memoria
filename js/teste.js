5ff786413a798602fe605cac3269a300e8bc0dd2
Felipe705 <delavedova44@gmail.com>
2023-04-19 17:28:41 -0300

    save

--- js/teste.js
+++ js/teste.js
@@ -1,8 +1,11 @@
-var express = require("express");
-var router = express.Router();
+const dados = require('../public/dados/testes.json') 
+
+const express = require("express");
+const router = express.Router();
+
 
 router.get('/', (req, res) => {
-    
+    res.sendFile(dados)
 })
 
 module.exports = router;
--- public/funcoes/funcoesCadastrarTeste.js
+++ public/funcoes/funcoesCadastrarTeste.js
@@ -1,15 +1,35 @@
 // import dados from '../dados/testes.json' assert { type: "json" };
-import funDados from '../../js/teste'
+// import funDados from '../../js/teste'
 // const fs = require(".//fs")
 
 
 // const dados = fs.readFile('../dados/teste.json')
 
-function cadastrarTeste(){
-    console.log(dados)
+// function cadastrarTeste(){
+//     console.log(dados)
+// }
+
+
+// cadastrarTeste()
+
+// console.log("dd")
+
+async function getConection() {
+    try {
+        const response = await fetch('http://localhost:8081/')
+       
+        const data = await response.json();
+        
+        console.log(response) 
+        console.log(data)
+
+        // for(let vaga of data) {
+        //     mudarStatus(vaga.idVaga, vaga.disponibilidade)
+        // }
+        
+    } catch(erro) {
+        console.log(erro);
+    }
 }
 
-
-cadastrarTeste()
-
-console.log("dd")
+getConection()
