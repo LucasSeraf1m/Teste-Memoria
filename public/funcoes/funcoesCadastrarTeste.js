2ded22cf1f6778db41a35af30c8dbaee9b40d0c5
Felipe705 <delavedova44@gmail.com>
2023-04-19 16:13:44 -0300

    puxar arquivo dados/teste.json

--- public/funcoes/funcoesCadastrarTeste.js
+++ public/funcoes/funcoesCadastrarTeste.js
@@ -1,9 +1,11 @@
-const fs = require('fs')
+import dados from '../dados/testes.json' assert { type: "json" };
+const fs = require
+
 
 // const dados = fs.readFile('../dados/teste.json')
 
 function cadastrarTeste(){
-    console.log("dados")
+    console.log(dados)
 }
 
 
