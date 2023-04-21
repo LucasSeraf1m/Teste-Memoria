d91de4462087de8ddd2e5821b1028344eabb537d
Felipe705 <delavedova44@gmail.com>
2023-04-20 22:18:45 -0300

    Inicio cadastro teste

--- js/route_testes.js
+++ js/route_testes.js
@@ -8,6 +8,9 @@ router.get('/', (req, res) => {
     res.send(dados)
 })
 
+router.post('/addTeste', (req, res) => {
+    const { title } = req.body
+})
 // fs.writeFile('../public/dados/teste.json', JSON.stringify(json), 'utf-8', (err) => {
 //     if (err) throw err;
 //     console.log('The file has been saved!');
