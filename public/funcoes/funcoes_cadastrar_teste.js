36a8da7ce7fcf473b8f7d38967bb79c49d5d84e1
DanielJunkes <danielmafioleti2@gmail.com>
2023-04-20 22:14:10 -0300

    n sei oq esta sendo comitado

--- public/funcoes/funcoes_cadastrar_teste.js
+++ public/funcoes/funcoes_cadastrar_teste.js
@@ -1,17 +1,10 @@
 async function getTeste() {
     try {
-        const response = await fetch('http://localhost:8080')
+        const response = await fetch('http://localhost:8081')
         const data = await response.json();
         
         for(let pergunta of data) {
             console.log(pergunta.title)
-            console.log(pergunta.perguntas.tituloDaPergunta)
-            console.log(pergunta.perguntas.opcaoA)
-            console.log(pergunta.perguntas.opcaoB)
-            console.log(pergunta.perguntas.opcaoC)
-            console.log(pergunta.perguntas.opcaoD)
-            console.log(pergunta.perguntas.opcaoE)
-            console.log(pergunta.perguntas.resporta)
         }
     } catch(erro) {
         console.log(erro);
