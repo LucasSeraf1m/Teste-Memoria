35addbd2b8c9bb711247651636f06b3d8013c07e
DanielJunkes <danielmafioleti2@gmail.com>
2023-04-21 16:10:55 -0300

    cadastro de perguntas

--- public/funcoes/funcoes_cadastrar_perguntas.js
+++ public/funcoes/funcoes_cadastrar_perguntas.js
@@ -3,7 +3,12 @@ const url = window.location.pathname
 const parts = url.split('/')
 const nomeTeste = parts[3]
 
-console.log(nomeTeste)
+const nome = document.createTextNode(nomeTeste)
+const h = document.getElementById("nome")
+
+h.appendChild(nome)
+
+var numPergunta = 1
 
 async function getTeste() {
     try {
@@ -11,9 +16,42 @@ async function getTeste() {
         const data = await response.json();
         
         for(let teste of data) {
-            
+            if(teste.title === nomeTeste){
+                
+            }
         }
     }catch(erro) {
         console.log(erro);
     }
 }
+function addPergunta(){
+
+    numPergunta++;
+
+    const sec = document.getElementById("sec")
+    const ori = document.getElementById("art1")
+    const clone = ori.cloneNode(true)
+
+    ori.setAttribute("id", "art"+numPergunta)
+
+    sec.appendChild(clone)
+
+}
+function salvarPergunta(){
+
+    var perguntas = document.getElementById("").value
+    console.log(perguntas)
+    const test = {
+        "title": nomeTeste,
+        "perguntas" : {
+            "tituloDaPergunta": "",
+            "opcaoA": "",
+            "opcaoB": "",
+            "opcaoC": "",
+            "opcaoD": "",
+            "opcaoE": "",
+            "resposta": ""
+        }
+    }
+}
+getTeste()
