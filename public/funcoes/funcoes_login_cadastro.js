4de9eefc3b72b28c5f62f045e01a4bf0e36b214c
LucasSeraf1m <lucasteixeiraserafim@gmail.com>
2023-04-20 21:39:17 -0300

    login e cadastro funcionais

--- public/funcoes/funcoes_login_cadastro.js
+++ public/funcoes/funcoes_login_cadastro.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 function loginUsuario() {
     const email = document.getElementById("emailInputLogin").value;
     const senha = document.getElementById("senhaInputLogin").value;
@@ -11,6 +13,31 @@ function loginUsuario() {
         if(!response.ok){
             throw new Error('Response error');
         }
+        window.location = 'http://localhost:8080/login/cadastrodetestes';
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log('Erro com a operação fetch', error);
+    });
+}
+
+function cadastroUsuario() {
+    const email = document.getElementById("emailInputCadastro").value;
+    const senha = document.getElementById("senhaInputCadastro").value;
+
+    fetch('http://localhost:8080/auth/signup', {
+        method: "POST",
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({"email": email, "senha": senha})
+    })
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Response error');
+        }
+        window.location = 'http://localhost:8080/login/cadastrodetestes';
         return response.json();
     })
     .then(data => {
