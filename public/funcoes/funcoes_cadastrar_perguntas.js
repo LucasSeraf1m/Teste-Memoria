a493d023218d6dc3265372a896b60df68ac681a5
DanielJunkes <danielmafioleti2@gmail.com>
2023-04-21 16:47:40 -0300

    Criar Form de Perguntas Dinamicamente

--- public/funcoes/funcoes_cadastrar_perguntas.js
+++ public/funcoes/funcoes_cadastrar_perguntas.js
@@ -8,7 +8,7 @@ const h = document.getElementById("nome")
 
 h.appendChild(nome)
 
-var numPergunta = 1
+var numPergunta = 0
 
 async function getTeste() {
     try {
@@ -29,13 +29,85 @@ function addPergunta(){
     numPergunta++;
 
     const sec = document.getElementById("sec")
-    const ori = document.getElementById("art1")
-    const clone = ori.cloneNode(true)
+    const art = document.createElement("article")
+    art.setAttribute("id", "art"+numPergunta)
+    const formu = document.createElement("form")
+    const titlePergunta = document.createElement("input")
+    titlePergunta.type = "text"
+    titlePergunta.setAttribute("id", "pergunta"+numPergunta)
+    const txtPergunta = document.createElement("label")
+    const textPergunta = document.createTextNode("Pergunta "+numPergunta)
+    txtPergunta.setAttribute("for", "titlePergunta")
+    const opA = document.createElement("input")
 
-    ori.setAttribute("id", "art"+numPergunta)
+    opA.type = "text"
+    opA.setAttribute("id", "opA"+numPergunta)
+    const txtopA = document.createElement("label")
+    const textopA = document.createTextNode("Opcão A")
+    txtopA.setAttribute("for", "opA"+numPergunta)
 
-    sec.appendChild(clone)
+    const opB = document.createElement("input")
+    opB.type = "text"
+    opB.setAttribute("id", "opB"+numPergunta)
+    const txtopB = document.createElement("label")
+    const textopB = document.createTextNode("Opcão B")
+    txtopB.setAttribute("for", "opB"+numPergunta)
 
+    const opC = document.createElement("input")
+    opC.type = "text"
+    opC.setAttribute("id", "opC"+numPergunta)
+    const txtopC = document.createElement("label")
+    const textopC = document.createTextNode("Opcão C")
+    txtopC.setAttribute("for", "opC"+numPergunta)
+
+    const opD = document.createElement("input")
+    opD.type = "text"
+    opD.setAttribute("id", "opD"+numPergunta)
+    const txtopD = document.createElement("label")
+    const textopD = document.createTextNode("Opcão D")
+    txtopD.setAttribute("for", "opD"+numPergunta)
+
+    const opE = document.createElement("input")
+    opE.type = "text"
+    opE.setAttribute("id", "opE"+numPergunta)
+    const txtopE = document.createElement("label")
+    const textopE = document.createTextNode("Opcão E")
+    txtopE.setAttribute("for", "opE"+numPergunta)
+
+    const opCerta = document.createElement("input")
+    opCerta.type = "text"
+    opCerta.setAttribute("id", "opCerta"+numPergunta)
+    const txtopCerta = document.createElement("label")
+    const textopCerta = document.createTextNode("Resposta Correta")
+    txtopCerta.setAttribute("for", "opCerta"+numPergunta)
+
+
+    txtPergunta.appendChild(textPergunta)
+    txtopA.appendChild(textopA)
+    txtopB.appendChild(textopB)
+    txtopC.appendChild(textopC)
+    txtopD.appendChild(textopD)
+    txtopE.appendChild(textopE)
+    txtopCerta.appendChild(textopCerta)
+
+    formu.appendChild(txtPergunta)
+    formu.appendChild(titlePergunta)
+    formu.appendChild(txtopA)
+    formu.appendChild(opA)
+    formu.appendChild(txtopB)
+    formu.appendChild(opB)
+    formu.appendChild(txtopC)
+    formu.appendChild(opC)
+    formu.appendChild(txtopD)
+    formu.appendChild(opD)
+    formu.appendChild(txtopE)
+    formu.appendChild(opE)
+    formu.appendChild(txtopCerta)
+    formu.appendChild(opCerta)
+    
+    art.appendChild(formu)
+    sec.appendChild(art)
+                
 }
 function salvarPergunta(){
 
