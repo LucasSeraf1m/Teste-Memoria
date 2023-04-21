5ce2eec46d0ccd7a451a30bd78e3623899394ddb
Felipe705 <delavedova44@gmail.com>
2023-04-20 22:12:36 -0300

    ARRUMADO rora 8081

--- public/funcoes/funcoes_cadastrar_teste.js
+++ public/funcoes/funcoes_cadastrar_teste.js
@@ -1,17 +1,10 @@
 async function getTeste() {
     try {
-        const response = await fetch('http://localhost:8080')
+        const response = await fetch('http://localhost:8081')
         const data = await response.json();
         
-        for(let pergunta of data) {
-            console.log(pergunta.title)
-            console.log(pergunta.perguntas.tituloDaPergunta)
-            console.log(pergunta.perguntas.opcaoA)
-            console.log(pergunta.perguntas.opcaoB)
-            console.log(pergunta.perguntas.opcaoC)
-            console.log(pergunta.perguntas.opcaoD)
-            console.log(pergunta.perguntas.opcaoE)
-            console.log(pergunta.perguntas.resporta)
+        for(let teste of data) {
+            console.log(teste.title)
         }
     } catch(erro) {
         console.log(erro);
@@ -21,8 +14,22 @@ async function getTeste() {
 function setTeste() {
     const title = "title";
 
-    fetch('http://localhost:8081/addTeste');
+    fetch('http://localhost:8081/addTeste', {
+        method: "post",
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({"title": title})
+    }).then(response => {
+        if(!response.ok){
+            throw new Error('Response error');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log('Erro com a operação fetch', error);
+    });
 }
 
 setTeste()
-getTeste()
