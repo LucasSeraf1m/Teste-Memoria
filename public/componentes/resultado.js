df0a70c2cf1ba158402a6a06221826f48a6f7102
Felipe705 <delavedova44@gmail.com>
2023-04-26 16:56:39 -0300

    Listar resultado

--- public/componentes/resultado.js
+++ public/componentes/resultado.js
@@ -1,5 +1,11 @@
-export function resultado() {
+export default function resultado(indice, result) {
     return `
-        h1
+        <div id="result${indice}">
+            <h2>${result.teste}</h2>
+            <h3>Quantidade de perguntas: </h3>
+            <p>${result.qtd_perguntas}</p>
+            <h3>Quantidade de acertos:</h3>
+            <p>${result.qtd_acertos}</p>
+        </div>
     `
 }
