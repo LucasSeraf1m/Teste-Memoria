ef13d29c954334a3a164a0c32e17c137dd81c037
Felipe705 <delavedova44@gmail.com>
2023-04-25 21:27:16 -0300

    ajustes

--- public/funcoes/funcoes_login_cadastro.js
+++ public/funcoes/funcoes_login_cadastro.js
@@ -2,46 +2,54 @@ function loginUsuario() {
     const email = document.getElementById("emailInputLogin").value;
     const senha = document.getElementById("senhaInputLogin").value;
     
-    fetch('http://localhost:8080/auth/signin', {
-        method: "POST",
-        headers: {'Content-type': 'application/json'},
-        body: JSON.stringify({"email": email, "senha": senha})
-    })
-    .then(response => {
-        if(!response.ok){
-            throw new Error('Response error');
-        }
-        window.location = 'http://localhost:8080/login/cadastrodetestes';
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log('Erro com a operação fetch', error);
-    });
+    if(email!="" && senha!="") {
+        fetch('http://localhost:8080/auth/signin', {
+            method: "POST",
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({"email": email, "senha": senha})
+        })
+        .then(response => {
+            if(!response.ok){
+                throw new Error('Response error');
+            }
+            window.location = 'http://localhost:8080/login/cadastrodetestes';
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log('Erro com a operação fetch', error);
+        });
+    }  else {
+        alert("Senha e ou email estão vazios")
+    }
 }
 
 function cadastroUsuario() {
     const email = document.getElementById("emailInputCadastro").value;
     const senha = document.getElementById("senhaInputCadastro").value;
 
-    fetch('http://localhost:8080/auth/signup', {
-        method: "POST",
-        headers: {'Content-type': 'application/json'},
-        body: JSON.stringify({"email": email, "senha": senha})
-    })
-    .then(response => {
-        if(!response.ok){
-            throw new Error('Response error');
-        }
-        window.location = 'http://localhost:8080/login/cadastrodetestes';
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log('Erro com a operação fetch', error);
-    });
+    if(email!="" && senha!="") {
+        fetch('http://localhost:8080/auth/signup', {
+            method: "POST",
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({"email": email, "senha": senha})
+        })
+        .then(response => {
+            if(!response.ok){
+                throw new Error('Response error');
+            }
+            window.location = 'http://localhost:8080/login/cadastrodetestes';
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log('Erro com a operação fetch', error);
+        });
+    } else {
+        alert("Senha e ou email estão vazios")
+    }
 }
